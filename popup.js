2f4010e6f568a01394367e75f369a548eca48f8d
alexwriter2026 <alex.writer2003@gmail.com>
2025-08-07 13:15:09 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,8 +3,10 @@ class PopupGeneratorWidget extends HTMLElement {
     const iframe = document.createElement('iframe');
     iframe.src = 'https://popup-generator.netlify.app/';
     iframe.style.width = '100%';
-    iframe.style.height = '900px';
+    iframe.style.height = '1000px';
     iframe.style.border = 'none';
     this.appendChild(iframe);
   }
 }
+
+customElements.define('popup-generator-widget', PopupGeneratorWidget);
